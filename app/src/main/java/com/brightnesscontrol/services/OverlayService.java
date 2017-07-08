a646e911f373abbcca5f53b5240840d2349ad678
vinayadahal <vinayadahal>
2017-07-08 19:32:42 +0545

    fullscreen brightness filter working

--- app/src/main/java/com/brightnesscontrol/services/OverlayService.java
+++ app/src/main/java/com/brightnesscontrol/services/OverlayService.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
@@ -16,6 +18,13 @@ public class OverlayService extends Service {
 
     MainActivity objMainActivity = new MainActivity();
     StaticVariables objStaticVariable = new StaticVariables();
+
+    final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
     @Override
     public IBinder onBind(Intent i) {
@@ -29,6 +38,7 @@ public class OverlayService extends Service {
         objStaticVariable.oView = new LinearLayout(this);
         objStaticVariable.oView.setBackgroundColor(0xdd000000); // The translucent black color
         objStaticVariable.oView.getBackground().setAlpha(Integer.parseInt(objMainActivity.readAlphaFromFile()));
+        objStaticVariable.oView.setSystemUiVisibility(flags);
 
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
